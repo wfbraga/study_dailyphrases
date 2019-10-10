b85f91de6f18816e0251826019b43eab6b3ff3aa
Wagner Braga <wfbraga@hotmail.com>
2019-10-10 14:18:05 -0300

    Storing Phrases in An Array

---
+++ build/app/generated/source/buildConfig/debug/com/redlike/daily_phrases/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.redlike.daily_phrases;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.redlike.daily_phrases";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
